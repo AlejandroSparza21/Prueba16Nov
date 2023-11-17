c1fd6b8ac2e9450abe56f83ea5c2228c3fee784c
Sergio Alejandro Esparza González <A01625430@tec.mx>
2023-11-16 18:09:15 -0600

    Creación de clases iniciales.

---
+++ Models/Demanda.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba16Nov.Models
+{
+    public class Demanda
+    {
+        public string IdCliente { get; set; }
+        public double OrigenLatitud { get; set; }
+        public double OrigenLonitud { get; set; }
+        public double DestinoLatitud { get; set; }
+        public double DestinoLonitud { get; set; }
+        public int CantidadKibros { get; set; }
+        public DateTime FechaCobertura { get; set; }
+
+        public Demanda() { }
+
+        public Demanda(string idCliente, double origenLatitud, double origenLonitud, double destinoLatitud, double destinoLonitud, int cantidadKibros, DateTime fechaCobertura)
+        {
+            IdCliente = idCliente;
+            OrigenLatitud = origenLatitud;
+            OrigenLonitud = origenLonitud;
+            DestinoLatitud = destinoLatitud;
+            DestinoLonitud = destinoLonitud;
+            CantidadKibros = cantidadKibros;
+            FechaCobertura = fechaCobertura;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ namespace Prueba16Nov
 {
     internal static class Program
     {
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
